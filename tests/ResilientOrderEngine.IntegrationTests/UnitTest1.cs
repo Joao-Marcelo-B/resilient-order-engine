4386005cd27c7be2a6196caa9c12491eaf128910
joao-marcelo-linux <joaomarcelobatistanarciso@gmail.com>
2026-09-01 22:11:15 -0300

    feat: adicionando estrutura do clean architecture

---
+++ tests/ResilientOrderEngine.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ResilientOrderEngine.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/ResilientOrderEngine.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ResilientOrderEngine.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
